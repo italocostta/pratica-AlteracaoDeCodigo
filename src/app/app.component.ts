eb858cbcd7a45baad4a05ded3afd33df1312b099
Gustavo Wagner <gugawag@gmail.com>
2023-04-03 13:50:08 -0300

    versao inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Usuario} from './shared/modelo/usuario';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeiro-angular-alunos';
+
+  usuario: Usuario;
+  usuarios: Array<Usuario>;
+  constructor() {
+    this.usuario = new Usuario();
+    this.usuarios = [
+      new Usuario('u1', 20),
+      new Usuario('u2', 24),
+      new Usuario('u3', 25)
+    ];
+  }
+
+  cadastrar(): void {
+    this.usuarios.push(this.usuario);
+    this.usuario = new Usuario();
+  }
 }
---
+++ src/app/shared/modelo/usuario.ts
@@ -0,0 +1,5 @@
+export class Usuario {
+
+  constructor(public nome='', public idade = 0) {
+  }
+}
